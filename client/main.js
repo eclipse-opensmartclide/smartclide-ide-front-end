2f9aaaff43457b5372b36d23e5720832e1e5c8b6
Pedro Ferreira <pedro.ferreira@unparallel.pt>
2021-08-06 16:00:19 +0100

    add initial login page

--- client/main.js
+++ client/main.js
@@ -1,9 +1,13 @@
 import Vue from 'vue'
-
 import '../imports/ui/plugins'
 import '../styles/variables.scss'
+import {BootstrapVue, BootstrapVueIcons, ImagePlugin} from "bootstrap-vue"
 
 import App from '../imports/ui/App.vue'
+
+Vue.use(BootstrapVue)
+Vue.use(BootstrapVueIcons)
+Vue.use(ImagePlugin)
 
 Meteor.startup(() => {
   new Vue({
--- imports/ui/App.vue
+++ imports/ui/App.vue
@@ -1,44 +1,18 @@
 <template>
-  <div class="container">
-    <header>
-      <h1>SmartCLIDE IDE</h1>
-    </header>
-    <ul>
-<!--      <Login/>-->
-      <Home/>
-<!--      <Task-->
-<!--          v-for="task in getTasks()"-->
-<!--          v-bind:key="task._id"-->
-<!--          v-bind:task="task"-->
-<!--      />-->
-    </ul>
-
-  </div>
+  <Login/>
 </template>
 
 <script>
-import Task from "./components/_old/Task.vue";
-import Hello from "/imports/ui/components/_old/Hello.vue"
-import Home from "/imports/ui/components/Home"
 import Login from "./components/Login";
 export default {
   components: {
     Login,
-    Task,
-    Hello,
-    Home
   },
   data() {
     return {};
   },
   methods: {
-    getTasks() {
-      return [
-        { _id: 1, text: "This is task 1" },
-        { _id: 2, text: "This is task 2" },
-        { _id: 3, text: "This is task 3" }
-      ];
-    }
+
   }
 };
 </script>
