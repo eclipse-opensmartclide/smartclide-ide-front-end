84d46570d454ba17c2f52df209a98a4d0997ea45
Goncalo Rolo <goncalo.rolo@unparallel.pt>
2022-07-20 16:10:12 +0100

    Update Credentials component

--- imports/ui/components/MyAccount/Credentials.vue
+++ imports/ui/components/MyAccount/Credentials.vue
@@ -10,33 +10,48 @@
 
 <template>
   <div>
-    <smart-widget-grid :draggable="false" :resizable="false" :layout.sync="cards" :margin="[25, 25]">
+    <smart-widget-grid isStatic :layout.sync="cards" :margin="[25, 25]" autoSize>
       <smart-widget class="title text-primary" v-for="card in cards" :slot="card.i" :title="card.category" :padding="[0,0]">
-        <CustomTable :rows="items" :columns="Object.keys(items[0])"/>
+        <BTable class="custom_table" hover bordered :items="items[card.category]"/>
       </smart-widget>
     </smart-widget-grid>
   </div>
 </template>
 
 <script>
-import CustomTable from "../Utils/CustomTable";
-
 export default {
   name: "Credentials",
-  components: {CustomTable},
   data(){
     return {
       cards: [
-        { x: 0, y: 0, w: 12, h: 5, i: 0, category: 'workflows'},
-        { x: 0, y: 11, w: 12, h: 5, i: 1, category: 'services'},
-        { x: 0, y: 24, w: 12, h: 5, i: 2, category: 'deployments'}
+        { x: 0, y: 0, w: 12, h: 5, i: 0, category: 'Git'},
+        { x: 0, y: 11, w: 12, h: 5, i: 1, category: 'Service Registries'},
+        { x: 0, y: 24, w: 12, h: 5, i: 2, category: 'Deployment Platforms'},
+        { x: 0, y: 36, w: 12, h: 5, i: 3, category: 'CI Managers'}
       ],
-      items: [
-        { age: 40, first_name: 'Dickerson', last_name: 'Macdonald' },
-        { age: 21, first_name: 'Larsen', last_name: 'Shaw' },
-        { age: 89, first_name: 'Geneva', last_name: 'Wilson' },
-        { age: 38, first_name: 'Jami', last_name: 'Carney' }
-      ]
+      items: {
+        "Git": [
+          { platform: "GitHub", URL: 'https://github.com', username: 'userA', token: "**********" },
+          { platform: "GitLab", URL: 'https://about.gitlab.com', username: 'userB', token: "**********" },
+          { platform: "Bitbucket", URL: 'https://bitbucket.org', username: 'userC', token: "**********" }
+        ],
+        "Service Registries": [
+          { type: "GitHub", URL: 'https://github.com', username: 'userD', token: "**********" },
+          { type: "GitLab", URL: 'https://about.gitlab.com', username: 'userE', token: "**********" },
+          { type: "Bitbucket", URL: 'https://bitbucket.org', username: 'userF', token: "**********" },
+          { type: "ProgrammableWeb", URL: 'https://www.programmableweb.com', username: 'userG', token: "**********" },
+          { type: "Docker", URL: 'https://www.docker.com/', username: 'userH', token: "**********" }
+        ],
+        "Deployment Platforms": [
+          { URL: 'https://kubernetes.io', username: 'userI', token: "**********" }
+        ],
+        "CI Managers": [
+          { type: "Jenkins", URL: 'https://www.jenkins.io', username: 'userJ', token: "**********" },
+          { type: "GitLab CI/CD", URL: 'https://about.gitlab.com', username: 'userK', token: "**********" },
+          { type: "GitHub Actions", URL: 'https://github.com', username: 'userL', token: "**********" },
+          { type: "Travis CI", URL: 'https://www.travis-ci.com/', username: 'userM', token: "**********" }
+        ],
+      }
     };
   },
   mounted(){
@@ -52,6 +67,7 @@ export default {
 
   .smartwidget{
     border-radius: 10px;
+    height: fit-content;
   }
 
   /deep/ .widget-header{
@@ -67,5 +83,9 @@ export default {
   /deep/ .custom_table thead{
     background: #E5EEFD;
     text-align: center;
+  }
+
+  /deep/ .custom_table tbody{
+    text-transform: none;
   }
 </style>
