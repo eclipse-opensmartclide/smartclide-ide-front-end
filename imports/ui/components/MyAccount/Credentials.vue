a57c36b1f2f04f65bdb9eae67f3e52308c009d80
Goncalo Rolo <goncalo.rolo@unparallel.pt>
2022-07-22 17:16:12 +0100

    Restructure the Credentials component

--- imports/ui/components/MyAccount/Credentials.vue
+++ imports/ui/components/MyAccount/Credentials.vue
@@ -9,10 +9,28 @@
  -------------------------------------------------------------------------------->
 
 <template>
-  <div>
-    <smart-widget-grid :colNum="1" :maxRows="4" :layout.sync="cards" :margin="[25, 25]" autoSize isStatic>
-      <smart-widget class="title text-primary" v-for="card in cards" :slot="card.i" :title="card.category" :loading="!items[card.category].loaded" :padding="[10,10]">
-        <BTable class="custom_table" :items="items[card.category].content" :fields="card.fields"/>
+  <div style="margin: 10px">
+    <smart-widget-grid
+      :colNum="1"
+      :maxRows="4"
+      :layout.sync="cards"
+      :margin="[25, 25]"
+      autoSize
+      isStatic
+    >
+      <smart-widget
+        class="title text-primary"
+        v-for="(table, index) in tables"
+        :slot="cards[index].i"
+        :title="table.title"
+        :loading="!table.loaded"
+        :padding="[10,10]"
+      >
+        <BTable
+          class="custom_table"
+          :items="table.content"
+          :fields="table.fields"
+        />
         <BButton class="add_button">Add new</BButton>
       </smart-widget>
     </smart-widget-grid>
@@ -25,29 +43,37 @@ export default {
   data(){
     return {
       cards: [
-        { x: 0, y: 0, w: 12, h: 5, i: 0, category: 'Git', fields: ["type", "URL", "username", "token"] },
-        { x: 0, y: 11, w: 12, h: 5, i: 1, category: 'Service Registries', fields: ["type", "URL", "username", "token"] },
-        { x: 0, y: 24, w: 12, h: 5, i: 2, category: 'Deployment Platforms', fields: ["URL", "username", "token"] },
-        { x: 0, y: 36, w: 12, h: 5, i: 3, category: 'CI Managers', fields: ["type", "URL", "username", "token"] }
+        { i: 0, x: 0, y: 0, w: 12, h: 5 },
+        { i: 1, x: 0, y: 11, w: 12, h: 5 },
+        { i: 2, x: 0, y: 24, w: 12, h: 5 },
+        { i: 3, x: 0, y: 36, w: 12, h: 5 }
       ],
-      items: {
-        "Git": {
-          loaded: false,
-          content: []
-        },
-        "Service Registries": {
-          loaded: false,
-          content: []
-        },
-        "Deployment Platforms": {
-          loaded: false,
+      tables: [
+        {
+          title: "Git",
+          fields: ["type", "URL", "username", "token"],
           content: [],
+          loaded: false
         },
-        "CI Managers": {
-          loaded: false,
-          content: []
+        {
+          title: "Service Registries",
+          fields: ["type", "URL", "username", "token"],
+          content: [],
+          loaded: false
+        },
+        {
+          title: "Deployment Platforms",
+          fields: ["URL", "username", "token"],
+          content: [],
+          loaded: false
+        },
+        {
+          title: "CI Managers",
+          fields: ["type", "URL", "username", "token"],
+          content: [],
+          loaded: false
         }
-      }
+      ]
     };
   },
   mounted(){
@@ -72,7 +98,7 @@ export default {
             if(content.length === 0)
               content = [{ type: null, URL: null, username: null, token: null }];
 
-            this.items["Git"] = { loaded: true, content };
+            Object.assign(this.tables[0],{ loaded: true, content });
           }
         }
       );
@@ -88,7 +114,7 @@ export default {
             if(content.length === 0)
               content = [{ type: null, URL: null, username: null, token: null }];
 
-            this.items["Service Registries"] = { loaded: true, content };
+            Object.assign(this.tables[1],{ loaded: true, content });
           }
         }
       );
@@ -104,7 +130,7 @@ export default {
             if(content.length === 0)
               content = [{ URL: null, username: null, token: null }];
 
-            this.items["Deployment Platforms"] = { loaded: true, content };
+            Object.assign(this.tables[2],{ loaded: true, content });
           }
         }
       );
@@ -120,7 +146,7 @@ export default {
             if(content.length === 0)
               content = [{ type: null, URL: null, username: null, token: null }];
 
-            this.items["CI Managers"] = { loaded: true, content };
+            Object.assign(this.tables[3],{ loaded: true, content });
           }
         }
       );
