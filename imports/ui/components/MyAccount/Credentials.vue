779e9ddca2589c16f1f7ab8fb424ec362108149c
Goncalo Rolo <goncalo.rolo@unparallel.pt>
2022-07-28 15:40:37 +0100

    Adjust Credentials css

--- imports/ui/components/MyAccount/Credentials.vue
+++ imports/ui/components/MyAccount/Credentials.vue
@@ -39,7 +39,7 @@
         </div>
         <div class="d-flex flex-row">
           <b-table
-            class="custom-table mx-1 mb-0 text-center w-100"
+            class="custom-table mx-1 mb-0 text-center"
             bordered
             :items="table.content"
             :fields="table.fields"
