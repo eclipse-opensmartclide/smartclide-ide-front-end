c3901b0d6953b8ce470fada96072e1aec653fa2c
Goncalo Rolo <goncalo.rolo@unparallel.pt>
2022-08-01 10:37:29 +0100

    Implement Service Registries component

--- client/store.js
+++ client/store.js
@@ -53,7 +53,7 @@ export default new Vuex.Store({
                         options: [
                             { title: 'Profile', link: '/profile' },
                             { title: 'Team', link: '/notImplemented' },
-                            { title: 'Service Registries', link: '/notImplemented' },
+                            { title: 'Service Registries', link: '/service-registries' },
                             { title: 'Credentials', link: '/credentials' }
                         ]
                     }
--- imports/ui/components/MyAccount/ServiceRegistries.vue
+++ imports/ui/components/MyAccount/ServiceRegistries.vue
@@ -9,20 +9,125 @@
  -------------------------------------------------------------------------------->
 
 <template>
-  <div>
-    ServiceRegistries
+  <div class="mx-4 mt-4">
+    <b-card>
+      <div class="d-flex flex-row align-items-center pb-3">
+        <b-form-input
+            class="search_bar"
+            placeholder="Type to search..."
+            type="search"
+            v-model="table.filter"
+            :disabled="!table.loaded"
+        />
+        <b-pagination
+            class="mx-3 mb-0"
+            size="sm"
+            limit="3"
+            v-model="table.currentPage"
+            :total-rows="table.totalRows"
+            :per-page="table.perPage"
+            :disabled="table.disablePagination"
+        />
+        <b-icon-plus-circle role="button" variant="primary" font-scale="1.5" @click="add"/>
+      </div>
+      <div class="d-flex flex-row">
+        <b-table
+            class="custom-table text-center"
+            bordered
+            :items="table.content"
+            :fields="table.fields"
+            :filter="table.filter"
+            :busy="!table.loaded"
+            :per-page="table.perPage"
+            :current-page="table.currentPage"
+            @filtered="onFiltered"
+            :empty-text="`No ${table.title} credentials were configured yet.`"
+            :empty-filtered-text="`No ${table.title} credentials matched your search criteria.`"
+            show-empty
+        >
+          <template #table-busy>
+            <div class="text-center text-primary my-2">
+              <b-spinner class="align-middle"></b-spinner>
+              Loading...
+            </div>
+          </template>
+          <template #cell(actions)="data">
+            <div class="text-center">
+              <b-icon-pencil role="button" variant="primary" class="mx-2" font-scale="1.2" @click="edit"/>
+              <b-icon-trash role="button" variant="primary" class="mx-2" font-scale="1.2" @click="remove"/>
+            </div>
+          </template>
+        </b-table>
+      </div>
+    </b-card>
   </div>
 </template>
 
 <script>
 export default {
   name: "ServiceRegistries",
+  data(){
+    return {
+      table: {
+        title: "Service Registries",
+        fields: ["type", "URL", "username", "actions"],
+        content: [],
+        loaded: false,
+        filter: null,
+        totalRows: null,
+        perPage: 10,
+        currentPage: 1,
+        disablePagination: null
+      }
+    };
+  },
   mounted(){
     this.$store.state.context = 'my-account';
+    this.fetchContent();
+  },
+  methods: {
+    fetchContent(){
+      this.fetchServiceRegistriesCredentials();
+    },
+    fetchServiceRegistriesCredentials(){
+      Meteor.call("request", { operationId: "getServiceRegistries", token: this.$store.state.keycloak.token},
+          (error, result) => {
+            if(result){
+              let content = result?.body.map((item) => {
+                return { type: item.type, URL: item.url, username: item.username };
+              });
+
+              Object.assign(this.table,{ loaded: true, content, totalRows: content.length, disablePagination: !content.length });
+            }
+          }
+      );
+    },
+    onFiltered(filteredItems){
+      this.table.disablePagination = !filteredItems.length;
+      this.table.totalRows = filteredItems.length;
+      this.table.currentPage = 1;
+    },
+    add(){
+      console.log("Add");
+    },
+    edit(){
+      console.log("Edit");
+    },
+    remove(){
+      console.log("Remove");
+    }
   }
 }
 </script>
 
 <style scoped>
+.search_bar{
+  border-color: #dee2e6;
+}
 
+/deep/ .custom-table thead{
+  background: var(--info);
+  text-align: center;
+  text-transform: capitalize;
+}
 </style>
