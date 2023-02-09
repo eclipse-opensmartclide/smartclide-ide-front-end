8fd33683cb6f8a28573e19c75040bc5371e596e2
Goncalo Rolo <goncalo.rolo@unparallel.pt>
2023-02-09 09:45:21 +0000

    Update About

--- imports/ui/components/Help/About.vue
+++ imports/ui/components/Help/About.vue
@@ -14,42 +14,49 @@
     title="About SmartCLIDE IDE"
     hide-footer
   >
-    <b-row>
-      <b-col cols="3">
-        <b-img src="/assets/SmartCLIDERGBColor.png" fluid/>
-      </b-col>
-      <b-col class="pl-0" align="justify">
-        <div>
-          <p>
-            <b>SmartCLIDE IDE</b>
-          </p>
-        </div>
-        <div class="about-text">
-          <p>
-            The SmartCLIDE IDE is a Cloud-based IDE named after and developed under the scope
-            of the <a href="https://smartclide.eu" target="_blank">SmartCLIDE</a> research project.
-          </p>
-        </div>
-        <div class="about-text">
-          <p>
-            SmartCLIDE has received funding from the European Union&apos;s Horizon 2020
-            research and innovation programme under grant agreement no. 871177.
-          </p>
-        </div>
-        <div class="about-text">
-          <p>
-            The source code was submitted as an open-source platform extending Eclipse Theia, which originated the
-            <a href="https://projects.eclipse.org/projects/ecd.opensmartclide" target="_blank">Eclipse OpenSmartCLIDE</a>
-            project.
-          </p>
-        </div>
-        <div class="about-text">
-          <p>
-            Report an issue <a href="https://github.com/eclipse-opensmartclide/smartclide-ide-front-end/issues" target="_blank">here</a>.
-          </p>
-        </div>
-      </b-col>
-    </b-row>
+    <b-table-simple>
+      <b-row>
+        <b-col cols="3">
+          <b-img src="/assets/SmartCLIDERGBColor.png" fluid/>
+        </b-col>
+        <b-col class="pl-0" align="justify">
+          <div>
+            <p>
+              <b>SmartCLIDE IDE</b>
+            </p>
+          </div>
+          <div class="about-text">
+            <p>
+              The SmartCLIDE IDE is a Cloud-based IDE named after and developed under the scope
+              of the <a href="https://smartclide.eu" target="_blank">SmartCLIDE</a> research project.
+            </p>
+          </div>
+          <div class="about-text">
+            <p>
+              The source code was submitted as an open-source platform extending Eclipse Theia, which originated the
+              <a href="https://projects.eclipse.org/projects/ecd.opensmartclide" target="_blank">Eclipse OpenSmartCLIDE</a>
+              project.
+            </p>
+          </div>
+          <div class="about-text">
+            <p>
+              Report an issue <a href="https://github.com/eclipse-opensmartclide/smartclide-ide-front-end/issues" target="_blank">here</a>.
+            </p>
+          </div>
+        </b-col>
+      </b-row>
+      <b-row align-v="center">
+        <b-col cols="3">
+          <div class="d-flex flex-row align-items-center">
+            <b-img src="/assets/EUFlag.png" fluid width="75px" center/>
+          </div>
+        </b-col>
+        <b-col class="about-text pl-0" align="justify">
+          SmartCLIDE has received funding from the European Union&apos;s Horizon 2020
+          research and innovation programme under grant agreement no. 871177.
+        </b-col>
+      </b-row>
+    </b-table-simple>
   </b-modal>
 </template>
 
