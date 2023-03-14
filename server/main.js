7558c5491181a66f8e0d42d25ef991cf824a6ed5
Goncalo Rolo <goncalo.rolo@unparallel.pt>
2023-03-14 12:46:35 +0000

    Add server method to get supported architectural patterns

--- server/main.js
+++ server/main.js
@@ -161,6 +161,23 @@ Meteor.methods({
         }
     },
     // Architectural Pattern Selection
+    async getSupportedPatterns(keycloakToken){
+        const config = {
+            method: 'GET',
+            url: `${process.env.SMARTCLIDE_BACKEND_URL}/architectural-patterns/supported-patterns`,
+            headers: {
+                'Accept': 'application/json',
+                'Authorization': `Bearer ${keycloakToken}`,
+            }
+        };
+
+        try{
+            const res = await axios(config);
+            return res.data;
+        } catch(e){
+            throw e;
+        }
+    },
     async getAPSSurvey(keycloakToken){
         const config = {
             method: 'GET',
