8f69a6888ee83515b6f4648a935376de82b80ed6
Goncalo Rolo <goncalo.rolo@unparallel.pt>
2023-06-12 16:14:56 +0100

    Support alternate theme

---
+++ styles/themes.js
@@ -0,0 +1,36 @@
+export const default_primary="#2272ea"
+export const default_secondary ="#29abe2"
+export const default_success ="#7ac943"
+export const default_info="#e5eefd"
+export const default_warning="#f7931e"
+export const default_danger="#ff1d25"
+export const default_light="#f8f9fa"
+export const default_dark="#212529"
+export const default_header_image="/assets/SmartCLIDERGBColor.png"
+
+export const themes = {
+	"default":{
+		colors:{
+			primary:default_primary,
+			secondary:default_secondary,
+			success:default_success,
+			info:default_info,
+			warning:default_warning,
+			danger:default_danger,
+			light:default_light,
+			dark:default_dark
+		},
+		images: {
+			header:default_header_image
+		}
+	},
+	"iotcat_ide":{
+		colors:{
+			primary:"#c99c28",
+			secondary:"#CBCBCB"
+		},
+		images:{
+			header:"https://iot-catalogue.s3.amazonaws.com/internalImages/r4R9H6Gc535eEqTnp/logo.png"
+		}
+	}
+}
